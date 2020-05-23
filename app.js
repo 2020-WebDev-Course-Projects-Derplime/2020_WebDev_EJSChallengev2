b8e67772700548dc259c5437bc58e908830a4c69
Emil Dolorfino <edolorfino24@gmail.com>
2020-05-23 02:43:24 -0400

    Step 1 done

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //jshint esversion:6
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -13,8 +13,30 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+// Mongoose connection
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+};
+mongoose.connect("mongodb://localhost:27017/blogpostDB", options, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Connected to MongoDB!");
+  }
+});
+
+// Make Post Schema/Model
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
 
 let posts = [];
 
@@ -38,15 +60,16 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
 
-  posts.push(post);
+  const { postTitle, postBody } = req.body;
 
+  const newPost = new Post({
+    title: postTitle,
+    content: postBody
+  });
+
+  newPost.save();
   res.redirect("/");
-
 });
 
 app.get("/posts/:postName", function(req, res){
